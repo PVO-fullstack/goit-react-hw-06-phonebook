cd88a536129c3e83238755fa4cc246f0c536bca4
Viacheslav Plevako <cheryec@ukr.net>
2023-03-29 18:30:48 +0300

    add persist

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react'
 import { store } from './redux/store'
 import { Provider } from 'react-redux'
 import { App } from 'components/App/App';
+import { persistor } from './redux/store';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
       </Provider>
     </BrowserRouter>
   </React.StrictMode>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,21 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import { contactsSlice } from "./contactsSlice";
 import { filterSlice } from './filterSlice';
 
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedContactsReducer = persistReducer(persistConfig, contactsSlice.reducer)
+
 export const store = configureStore({
   reducer: {
-    contacts: contactsSlice.reducer,
+    contacts: persistedContactsReducer,
     filter: filterSlice.reducer,
   },
 })
+
+export const persistor = persistStore(store);
